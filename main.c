a37df111fd91d9cac84e0d518abbef6361eb63f4
Silicon014 <kaguya.h@foxmail.com>
2017-11-30 21:15:02 +0800

    Update

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #define STACKSIZE 100
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
 #include"binary_tree.h"
 static void title(void);
 static void end(void);
 char * GetInput(char []);
+void InToPost(char []);
 void ToTree(BinaryTree, char []);
+double Conclusion(BinaryTree);
 
 int main(void)
 {
@@ -15,13 +19,97 @@ int main(void)
 	title();
 	while (GetInput(stack) != NULL)
 	{
+		InToPost(stack);
 		ToTree(tree, stack);
 		printf("result: %.3lf\n", Conclusion(tree));
 	}
 	end();
 	return 0;
 }
-char * GetInput(char []);
+void InToPost(char stack[])
+{
+	;
+}
+void ToTree(BinaryTree tree, char source[])
+{
+	double tmp;
+	BinaryTree stack[STACKSIZE];
+	
+	while (*source != '\0')
+	{
+		BinaryTree t;
+
+		if (isdigit(*source))
+		{
+			tmp = strtod(source, NULL, 10);
+			while (isdigit(*source))
+				source++;
+			t = Create();
+			t->element.d = tmp;
+			*stack = t;
+			stack++;
+		}
+		else
+		{
+			t = Union(stack - 2, stack - 1);
+			t->element.ch = *source;
+			stack--;
+			*(stack - 1) = t;
+			source++;
+		}
+	}
+	tree->left = *stack;
+	tree->element.ch = '+';
+}  
+double Conclusion(BinaryTree tree)
+{
+	if (tree == NULL)
+		return 0;
+	if (tree->left == NULL && tree->right == NULL)
+		return tree->element.d;
+	else
+	{
+		double l = Conclusion(tree->left);
+		double r = Conclusion(tree->right);
+		
+		switch (tree->element.ch)
+		{
+			case '+':
+				return l + r;
+			case '-':
+				return l - r;
+			case '*':
+				return l * r;
+			case '/':
+				if (r == 0.0)
+					fprintf(stderr, "Error: division 0\n");
+				return l / r;
+			case '^':
+				return pow(l, r);
+			default:
+				break;
+		}
+	}
+	return 10000;
+}
+char * GetInput(char array[])
+{
+	char ch;
+	int i = 0;
+
+	ch = getchar();
+	while (ch != '\n' && ch != EOF)
+	{
+		array[i] = ch;
+		++i;
+		ch = getchar();
+	}
+	array[i] = '\0';
+	if (ch == EOF)
+		return NULL;
+	else
+		return array;
+}
 static void Starbar(void)
 {
 	const int Stars = 80;
